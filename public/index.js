3f884a7ba835c5e8224dfb3ab0b39537a02bbf99
Fabian Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2016-03-20 02:01:32 +0100

    Add toggles

--- public/index.js
+++ public/index.js
@@ -7,4 +7,21 @@ function render() {
     $('#' + key).append('<input class="key" type="text" disabled="true" value="' + key + '">');
     $('#' + key).append('<input class="value" type="text" disabled="true" value="' + data[key] + '">');
   };
+  $('#flash-toggle').click(function(){
+    if( $('#flash .value').val() == "true" ) {
+      $('#flash .value').val("false");
+    } else if( $('#flash .value').val() == "false" ) {
+      $('#flash .value').val("true");
+    }
+    saveProperties();
+  });
+  
+  $('#light-toggle').click(function(){
+    if( $('#light .value').val() == "true" ) {
+      $('#light .value').val("false");
+    } else if( $('#light .value').val() == "false" ) {
+      $('#light .value').val("true");
+    }
+    saveProperties();
+  });
 }
